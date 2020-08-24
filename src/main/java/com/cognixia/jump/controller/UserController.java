aa53e18e5bc8d791acdeca62ae7ec104b41c030a
Darreal Chambers <dchambers@Darreals-MacBook-Pro.local>
2020-08-24 16:19:54 -0400

    user requestmapping updated

--- src/main/java/com/cognixia/jump/controller/UserController.java
+++ src/main/java/com/cognixia/jump/controller/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@RequestMapping("/userApi")
+@RequestMapping("/api")
 @RestController
 public class UserController {
     @Autowired
