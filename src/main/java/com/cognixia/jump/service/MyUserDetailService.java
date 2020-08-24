ac628458db8f2849012a65525da326a5c3af8ea2
David Morales <davmorales@ucdavis.edu>
2020-08-24 13:13:04 -0700

    Added SecurityConfig.java and MyUserDetails.java, MyUserDetailService.java. Added index.html just to test if loggin in works.

---
+++ src/main/java/com/cognixia/jump/service/MyUserDetailService.java
@@ -0,0 +1,26 @@
+package com.cognixia.jump.service;
+
+import com.cognixia.jump.model.User;
+import com.cognixia.jump.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class MyUserDetailService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        Optional<User> userFound = userRepository.findByEmail(userName);
+        if(!userFound.isPresent()) {
+            throw new UsernameNotFoundException("Email " + userName + " does not exists");
+        }
+        return new MyUserDetails(userFound.get());
+    }
+}
---
+++ src/main/java/com/cognixia/jump/service/MyUserDetails.java
@@ -0,0 +1,62 @@
+package com.cognixia.jump.service;
+
+import com.cognixia.jump.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+public class MyUserDetails implements UserDetails {
+    private static final long serialVersionUID = 1L;
+
+    private String userName;
+    private String password;
+    private boolean enabled;
+    private List<GrantedAuthority> authorities;
+    public MyUserDetails() {}
+
+    public MyUserDetails(User user) {
+        this.userName = user.getEmail();
+        this.password = user.getPassword();
+        this.enabled = user.isEnable();
+        this.authorities = Arrays.asList(new SimpleGrantedAuthority(user.getUserRole().toString()));
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+}
